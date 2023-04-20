65a703def0125589c0c8f4c97661754167e90031
Cassiano de Souza <cassianodess@hotmail.com>
2023-04-20 19:57:35 -0300

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ProgWeb2023.1';
+export class AppComponent implements OnInit {
+  public title: string = 'ProgWeb2023.1';
+
+  public form: FormGroup = new FormGroup({
+    question: new FormControl(''),
+  });
+
+  
+  ngOnInit(): void {
+
+  }
+
+
+  public onSubmit(): void {
+    console.log(this.form.get("question")?.value);
+  }
 }
