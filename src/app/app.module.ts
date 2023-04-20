7cd7ed8e157aff24132695bf71fc5ccda6d4d5d4
Cassiano de Souza <cassianodess@hotmail.com>
2023-04-20 20:52:25 -0300

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SideButtonComponent } from './shared/side-button/side-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideButtonComponent
   ],
   imports: [
     BrowserModule,
